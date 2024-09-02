a69b428b502442913283a90e4fda22fc5650d3dc
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-09-02 09:49:46 +0100

    first commit init project

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<!-- Tample -->
+<template>
+  <div class="container">
+    <article class="card p-5">
+      <span>« </span>Tatouage Quand les mots se dérobent et que le silence
+      enveloppe mon être, ma peau devient mon ultime refuge, mon temple secret.
+      Les émotions muettes s'inscrivent en traces invisibles, gravées sur cette
+      toile vivante. Chaque battement de cœur murmure l'écho d'un je t'aime non
+      prononcé, un appel muet à l'âme qui peut lire ma peau. Voir sur
+      <span>»</span>
+      <hr />
+      <b></b>
+      <p>Sabbat Lumpatshia</p>
+    </article>
+  </div>
+</template>
+
+<!-- Script -->
+<script></script>
+
+<!-- Style -->
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Nerko+One&display=swap");
+/* .roboto-mono-<uniquifier > {
+  font-family: "Roboto Mono", monospace;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+} */
+
+/* .nerko-one-regular {
+  font-family: "Nerko One", cursive;
+  font-weight: 400;
+  font-style: normal;
+} */
+
+html,
+body {
+  background-color: #c6c6c6;
+}
+.container {
+  width: 80%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #cfd5e2;
+  margin-top: 50px;
+  border-radius: 5px;
+}
+.card {
+  width: 50%;
+  background-color: #fff;
+  color: black;
+  padding: 20px;
+  font-family: "Roboto Mono", monospace;
+  font-weight: 500;
+  font-style: italic;
+}
+
+span {
+  /* font-family: "Nerko One", cursive; */
+  font-size: 1.5em;
+  color: #84135e;
+  font-weight: bold;
+}
+
+p {
+  padding-top: 10px;
+  text-align: left;
+  font-family: "Nerko One", cursive;
+  color: grey;
+}
+b {
+  background-color: #84135e;
+  width: 10%;
+  height: 10%;
+}
+</style>
