0f18e6b8c11186bdc3a1e9bbb862b48af4860227
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-09-02 13:48:23 +0100

    add hover linear-gradient

--- src/App.vue
+++ src/App.vue
@@ -101,11 +101,17 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(to right, #cfd5e2, #8b49c8);
+  background: linear-gradient(#cfd5e2, #8b49c8);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-top: 50px;
   border-radius: 5px;
 }
+
+.container:hover {
+  cursor: pointer;
+  background: linear-gradient(to right, #8b49c8, #cfd5e2);
+}
+
 .card {
   width: 50%;
   background-color: #fff;
